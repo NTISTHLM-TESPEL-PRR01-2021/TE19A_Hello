52b5d3ee686d0ff083ae7cb7ef00231ac3e8d5aa
Mikael <mikael.bergstrom@ga.ntig.se>
2020-09-03 10:45:22 +0200

    Initial commit

---
+++ TE19A-Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TE19A_Hello
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      Console.WriteLine("What shall we do today?");
+
+      string name;
+
+      name = "Micke";
+
+      System.Console.WriteLine("My name is" + name);
+
+      Console.ReadLine();
+
+    }
+  }
+}
